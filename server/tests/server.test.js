1c36bf3d199a53a0253d3c041e03165cf3f8c268
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-29 23:55:50 -0400

    added test for get todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require("mongodb");
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -63,5 +66,27 @@ describe("Get /todos", () => {
     request(app).get("/todos").expect(200).expect((res) => {
       expect(res.body.todos.length).toBe(2);
     }).end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should return todo doc", (done) => {
+    let id = todos[0]._id.toHexString();
+    request(app).get("/todos/" + id).
+    expect(200).
+    expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    }).
+    end(done)
+  });
+
+  it("should return 404 if todo not found", (done) => {
+    let id = "5904088504c7d7848efccc3a";
+
+    request(app).get("/todos/" + id).expect(404).end(done);
+  });
+
+  it("should return 404 for non-object ids", (done) => {
+    request(app).get("/todos/123").expect(404).end(done);
   });
 });
