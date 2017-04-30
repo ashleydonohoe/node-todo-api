79f5c8bea51af5fefca5db96328b04c53956ae93
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-30 00:44:54 -0400

    added testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,7 @@ const todos = [{
   _id: new ObjectID(),
   text: 'First test todo'
 }, {
-  id: new ObjectID(),
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -88,5 +88,41 @@ describe("GET /todos/:id", () => {
 
   it("should return 404 for non-object ids", (done) => {
     request(app).get("/todos/123").expect(404).end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it("should return 404 if todo not found", (done) => {
+      let id = "5904088504c7d7848efccc3a";
+
+      request(app).delete(`/todos/${id}`).expect(404).end(done);
+
+  });
+
+  it("should return 404 for non-object id", (done) => {
+
+    request(app).delete("/todos/123").expect(404).end(done);
+
   });
 });
